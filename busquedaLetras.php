289c3d943fb0801ec4ef4173e5c1e1a754e3bd48
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-16 10:33:08 -0700

    Create busquedaLetras.php

---
+++ busquedaLetras.php
@@ -0,0 +1,88 @@
+<?php
+
+//update_last_activity.php
+
+require('abstract.databoundobject.php');
+require('mapping.php');
+require('class.pdofactory.php');
+
+$conexion = mysqli_connect('localhost', 'adri', 'root') or 
+    die ('Unable to connect. Check your connection parameters.');
+mysqli_select_db($conexion, 'buscador') or die(mysqli_error($conexion));
+
+$letra = $_POST['valorCaja1'];
+ 
+$sql = "SELECT * FROM search WHERE paraula = '$letra'";
+
+$result = mysqli_query($conexion, $sql) or die (mysqli_error($conexion));
+
+while ($row =mysqli_fetch_assoc($result)) {
+$ids = $row['id'];
+} 
+if (isset($ids)) {
+	$sql1 = "SELECT * FROM search  WHERE paraula LIKE '$letra%' ORDER BY total DESC LIMIT 5;";
+	$result1 = mysqli_query($conexion, $sql1) or die (mysqli_error($conexion));
+	while ($row =mysqli_fetch_assoc($result1)) {
+		$topBusquedas[] = $row['paraula'];
+	} 
+	echo $topBusquedas[0];
+	echo $topBusquedas[1];
+	echo $topBusquedas[2];
+	$sql = "UPDATE search SET total = total + 1 WHERE id = '$ids'";
+	$result = mysqli_query($conexion, $sql) or die (mysqli_error($conexion));
+}else{
+	$current_timestamp = strtotime(date("Y-m-d H:i:s") . '- 10 second');
+	$current_timestamp = date('Y-m-d H:i:s', $current_timestamp);
+	echo $current_timestamp;
+	$sql = "INSERT INTO search (paraula, total, lastvisit) VALUES ('$letra', 0, '$current_timestamp')";
+	$result = mysqli_query($conexion, $sql) or die (mysqli_error($conexion));
+}
+
+
+
+/*$strDSN = "pgsql:dbname=buscador;host=localhost;port=5432;user=postgres;password=";
+        $objPDO = PDOFactory::GetPDO($strDSN, "buscador", "", 
+            array());
+        $objPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $reps=0;
+        for ($cont=1; $cont < 5; $cont++) { 
+        	$objUser = new Search($objPDO, $cont);
+        	$letra[$cont] = $objUser->getparaula();
+			$objUser->gettotal();
+			$objUser->getlastvisit();
+			echo $objUser->getID();
+			unset($objUser);
+        }
+        for ($cont2=1; $cont2 < 5; $cont2++) { 
+	        if ($letra[$cont2] != $_POST['valorCaja1']) {
+					$reps++;
+			}
+		}
+		if($reps==4){
+			$current_timestamp = strtotime(date("Y-m-d H:i:s") . '- 10 second');
+		 	$current_timestamp = date('Y-m-d H:i:s', $current_timestamp);
+			$objUser2 = new Search($objPDO);
+			$objUser2->setparaula($_POST['valorCaja1']);
+			$objUser2->settotal(0);
+			$objUser2->setlastvisit($current_timestamp);
+			$objUser2->Save();
+			unset($objUser2);
+		}
+*/
+		/*$det = new Search($objPDO, $_SESSION["login_details_id"]);
+		$det->setis_type($_POST["is_type"]);
+		$det->Save();*/
+//print_r($_POST);
+//$query = "SELECT * FROM search WHERE paraula= ".$letra." ";
+
+/*$objUser->setparaula("Hola");
+$objUser->settotal(23);
+$objUser->setlastvisit("2001-09-28 01:00:00");
+$objUser->Save();
+
+        $id = $objUser->getID();
+        
+        
+        unset($objUser);*/
+?>
+
